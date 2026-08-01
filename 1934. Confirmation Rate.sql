60deb14ed86adbad31cff68c2dc0bccdc58ec594
Saranya A <asaranya131@gmail.com>
2026-08-01 18:47:13 +0530

    Add SQL query to calculate user confirmation rate

---
+++ 1934. Confirmation Rate.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT s.user_id,
+ 
+  ROUND(SUM(CASE WHEN c.action = "confirmed" THEN 1 ELSE 0 END )/COUNT(*),2) AS confirmation_rate
+
+FROM Signups as s
+LEFT JOIN Confirmations as c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
+ORDER BY confirmation_rate
+
